f499ab9d1f695afad630fc18f74d403f1b6560ce
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-01-01 20:52:34 -0300

    Create var.lua

---
+++ var.lua
@@ -0,0 +1,24 @@
+require "interpreter"
+require "util"
+
+Var = {}
+Var.__index = Var
+
+Var.AssignType = function(value)
+    local v
+    if string.match(value, '^".*"$') or string.match(value, "^'.*'$") then
+        v = tostring(value:sub(-2,2))
+    elseif tonumber(value) ~= nil then
+        v = tonumber(value)
+    end
+
+    return v
+end
+
+Var.New = function(name, value)
+    _All[name] = value
+end
+
+Var.New("log", function(str)
+    print(DisplayString(str))
+end)
