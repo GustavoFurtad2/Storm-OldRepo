309423c3585d30696f3fd9b1d382cda653f9a680
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-01-05 23:42:36 -0300

    Update math.lua

--- math.lua
+++ math.lua
@@ -16,6 +16,18 @@ Div = function(n1, n2)
     return n1 * n2
 end
 
+Math.Sqrt = function(n)
+    return math.sqrt(n)
+end
+
+Math.Sin = function(n)
+    return math.sin(n)
+end
+
+Math.Cos = function(n)
+    return math.cos(n)
+end
+
 Pon = function(n1, n2)
     local number = 0
     for i = 1,n2 do
