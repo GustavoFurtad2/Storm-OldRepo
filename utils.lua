f3063d2d94fcc3ddbdde4b4c57ae84a4ac47f3db
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-03-16 09:04:17 -0300

    Add concatenation and optimization

--- utils.lua
+++ utils.lua
@@ -7,24 +7,33 @@ end
 
 function ToValue(value)
    
-   if string.match(value, [[^["'].-["']$]]) then
+   if string.match(value, "[+%-*/]") or string.match(value, "%.%.") then
+
+      return load("return " .. value, nil, nil, _A)()
+   elseif string.match(value, [[^["'].-["']$]]) then
 
       return value:sub(2, -2)
+
    elseif value == "true" then
+
       return true
 
    elseif value == "false" then
+
       return false
 
    elseif tonumber(value) ~= nil then
 
       return tonumber(value)
+
    elseif _A[value] then
 
       return _A[value]
-   elseif string.match(value, [[^["'].-["']$]]) then
+
+   elseif not value:match("^[\"'].-[\"']$") then
       
       Error("Lexer", "Unfinished string")
+
    else
       return nil
    end
@@ -36,4 +45,25 @@ function Split(s, delimiter)
       table.insert(result, match)
     end
     return result
+end
+
+function ShowTokens()
+   local biggestSet = 1
+
+   for i, t in next, Tokens[Line] do
+      local length = tostring(t.Token):len()
+      if length > biggestSet then
+         biggestSet = length
+      end
+   end
+
+   print(" NUMBER |  SET" .. string.rep(" ", biggestSet) .. "| TYPE   ")
+
+   for i, t in next, Tokens[Line] do
+      print(string.format(" %s| %s| %s",
+        i .. string.rep(" ", 7 - tostring(i):len()),
+        t.Token .. string.rep(" ", (4 + biggestSet) - tostring(t.Token):len()),
+        t.Type)
+      )
+   end
 end
