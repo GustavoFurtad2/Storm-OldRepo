af13cc61e9f184658c85820e154a465a134098e8
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-03-16 10:35:41 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@ require "interpreter"
 require "lexer"
 
 os.execute "title Storm"
-print(string.format("[Storm %s - 2024] - https://github.com/GustavoFurtad2/Storm", _A.VERSION))
+print(string.format("[Storm %s - 2024] - https://github.com/GustavoFurtad2/Storm", _A._VERSION))
 
 Lexer(io.read())
