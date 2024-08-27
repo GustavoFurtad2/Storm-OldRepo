f60979714fdd54c64ef593fcd6d64918213ad2b5
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-26 23:44:57 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 require "interpreter/env"
 require "interpreter/lexer"
 
-os.execute "title Storm"
+os.execute("title Storm " .. _GLOBAL._VERSION)
 print("Storm " .. _GLOBAL._VERSION .. " - https://github.com/GustavoFurtad2/Storm")
 
 lexer(io.read())
