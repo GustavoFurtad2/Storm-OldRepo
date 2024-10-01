73cc5c9b5738510ac10ef1d882248e017aa684e2
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-30 23:14:17 -0300

    add debug mode, minor changes

--- interpreter/env.lua
+++ interpreter/env.lua
@@ -1,42 +1,56 @@
-onCallFunction  = false
-onFunction      = false
-onString        = false
-onScope         = false
-lastEmpty       = false
-crashed         = false
+debug = false
 
-currentChar     = nil
-lineLength      = nil
-lineCode        = nil
+onCallFunction = false
+onFunction = false
+onString = false
+onScope = false
+lastSpace = false
+crashed = false
+
+lineLength = 0
+lineCode = ""
+
+charIndex = 0
+currentLine = 1
+
+currentChar = nil
+currentToken = nil
+
+nextToken = nil
+nextNextToken = nil
+nextNextNextToken = nil
+
+lines = {}
+scopes = {}
 
 openParentheses = 0
-openScopes      = 0
-charIndex       = 0
+openScopes = 0
 
-set = ""
+currentScope = nil
 
-currentToken, nextToken, nextNextToken, nextNextNextToken = nil, nil, nil, nil
-tokens, scopes, lines, currentLine, currentScope = {}, {}, {}, 1, nil
+tokens = {}
+tokenSet = ""
 
 tokenType = {
-    ["Identifier"] = 1,
-    ["Function"]   = 2,
-    ["Keyword"]    = 3,
-    ["Equals"]     = 4,
-    ["Call"]       = 5,
-    ["End"]        = 6,
+    ["identifier"] = 1,
+    ["function"]   = 2,
+    ["assign"]     = 3,
+    ["args"]       = 4,
+    ["end"]        = 5
 }
 
 _GLOBAL = {
     _VERSION = "0.1.0",
-    _OS = package.config:sub(1,1) == "\\" and "Windows" or "Linux",
     _CPATH = arg[0],
-
-    print = function(output)
-        print(output)
-    end,
-
-    getTime = function()
-        return os.time()
-    end,
+    _OS = package.config:sub(1,1) == "\\" and "Windows" or "Linux",
 }
+
+_GLOBAL.print = function(output)
+
+    print(output)
+end
+
+_GLOBAL.read = function()
+
+    return io.read()
+end
