4ffe2aed628f2c6d365d54ad05ec842159a5701b
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-30 23:09:57 -0300

    minor changes

--- interpreter/scope.lua
+++ interpreter/scope.lua
@@ -1,27 +1,32 @@
 require "interpreter/env"
 
-function scope(name)
+Scope = {}
+Scope.__index = Scope
 
-    local scope = {
-        name = name,
-        instructions = {},
-    }
+function Scope:new(name)
+
+    local scope = setmetatable(
+        {
+            name = name,
+            instructions = {}
+        },
+    Scope)
 
     openScopes = openScopes + 1
-    onScope    = true
-
-    function scope:newInstruction(instruction)
-
-        table.insert(self.instructions, instruction)
-    end
-
-    function scope:execute()
-        
-        for k, v in next, self.instructions do
-            
-            v()
-        end
-    end
+    onScope = true
 
     return scope
+end
+
+function Scope:newInstruction(instruction)
+
+    table.insert(self.instructions, instruction)
+end
+
+function Scope:execute()
+
+    for i, instruction in next, self.instructions do
+
+        instruction()
+    end
 end
