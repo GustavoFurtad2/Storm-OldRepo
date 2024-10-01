99cc43c1b5f28e5fb40d3a48b919c814a0b135a8
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-30 23:13:53 -0300

    add some error handling, add function of token viewing in debug mode

--- interpreter/lexer.lua
+++ interpreter/lexer.lua
@@ -8,7 +8,7 @@ local function checkLine()
     if lineCode:match("^%s*%-%-") then
         return
     end
-  
+
     for char, index in lineCode:gmatch("(.)()") do
 
         if crashed then
@@ -18,13 +18,48 @@ local function checkLine()
         currentChar = char
         charIndex = charIndex + 1
         tokenize()
+        
+    end
+
+    if onCallFunction then
+
+        error("Type Error", "')' expected to close call function at line " .. currentLine)
+    elseif onString then
+        
+        error("Type Error", [[" or ' expected to close string at line ]] .. currentLine)   
+    end
+
+   if debug then
+
+        local biggestSet = 1
+
+        for i, token in next, tokens[currentLine] do
+
+            local length = tostring(token.value):len()
+
+            if length > biggestSet then
+
+                biggestSet = length
+            end
+        end
+
+        print(" NUMBER |  SET" .. string.rep(" ", biggestSet) .. "| TYPE   ")
+
+        for i, token in next, tokens[currentLine] do
+
+            print(string.format(
+                        " %s| %s| %s", i .. string.rep(" ", 7 - tostring(i):len()),
+                        token.value .. string.rep(" ", (4 + biggestSet) - tostring(token.value):len()),
+                        token.type
+            ))
+        end
     end
 end
 
 function lexer(sourceCode)
 
     if sourceCode:len() > 0 then
-        
+
         for line in string.gmatch(sourceCode, "[^\n]+") do
             table.insert(lines, line)
         end
@@ -42,10 +77,12 @@ function lexer(sourceCode)
 
             checkLine()
             parser()
+        
         end
 
         if openScopes > 0 then
-            error("Type Error", "'end' expected to close function")
+
+            error("Type Error", "'end' expected at line " .. currentLine)     
         end
     end
 end
