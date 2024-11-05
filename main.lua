07910a93577c318c4394af1e1677207e527fe334
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-11-04 22:31:04 -0300

    add function of loading standard libraries

--- main.lua
+++ main.lua
@@ -1,6 +1,20 @@
 require "interpreter/env"
 require "interpreter/lexer"
 
+local getFilesCommand = _GLOBAL._OS == "Windows" and "dir /b" or "ls"
+
+local function loadStandartLibs()
+
+    local files = io.popen("cd stdlibs && " .. getFilesCommand):lines()
+
+    for fileName in files do
+
+        require("stdlibs/" .. fileName:sub(1, fileName:len() - 4))
+    end
+end
+
+loadStandartLibs()
+
 os.execute("title Storm " .. _GLOBAL._VERSION)
 print("Storm " .. _GLOBAL._VERSION .. " - https://github.com/GustavoFurtad2/Storm")
 
